47567943816ce30bcdc934f4c41b0de597b7e2f8
dev_nayan <md.nayan8494@gmail.com>
2022-04-18 09:57:39 +0600

    Some modification on Services section

--- src/components/Home/Services/Service/Service.js
+++ src/components/Home/Services/Service/Service.js
@@ -2,20 +2,19 @@ import React from 'react'
 import { Card } from 'react-bootstrap'
 import './Service.css'
 
-const Services = ({service}) => {
-  const {name,img, details} = service;
+const Services = ({ service }) => {
+  const { name, img, price, details } = service
   return (
     <Card>
       <Card.Img variant="top" src={img} />
       <Card.Body>
-        <Card.Title>{name}</Card.Title>
-        <Card.Text>
-          {details}
-        </Card.Text>
+        <div className="d-flex justify-content-between align-items-center">
+          <Card.Title>{name}</Card.Title>
+          <h3 className='service-price'>${price}<span style={{fontSize: '16px'}}>/month</span></h3>
+        </div>
+        <Card.Text>{details}</Card.Text>
       </Card.Body>
-      <button className='service-btn'>
-        Book a Session
-      </button>
+      <button className="service-btn">Book a Session</button>
     </Card>
   )
 }
